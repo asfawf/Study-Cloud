474391be514322c6ba61b519a9f43a7b5a7aab30
asfawf <soub0713@naver.com>
2023-03-20 14:09:46 +0900

    vo Lombok 으로 전환
    오류 없음

--- src/main/java/study/cloud/stc/test/model/vo/TestVo.java
+++ src/main/java/study/cloud/stc/test/model/vo/TestVo.java
@@ -4,7 +4,19 @@ import java.sql.Date;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Component
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class TestVo {
 	
 	/*
@@ -28,104 +40,80 @@ public class TestVo {
 	
 	
 	
-	public TestVo() {
-		super();
-	}
-
-
-
-	public TestVo(int boardNum, String boardWriter, String boardSubject, String boardContent, Date boardDate,
-			int boardView) {
-		super();
-		this.boardNum = boardNum;
-		this.boardWriter = boardWriter;
-		this.boardSubject = boardSubject;
-		this.boardContent = boardContent;
-		this.boardDate = boardDate;
-		this.boardView = boardView;
-	}
-
-
-
-	public int getBoardNum() {
-		return boardNum;
-	}
-
-
-
-	public void setBoardNum(int boardNum) {
-		this.boardNum = boardNum;
-	}
-
-
-
-	public String getBoardWriter() {
-		return boardWriter;
-	}
-
-
-
-	public void setBoardWriter(String boardWriter) {
-		this.boardWriter = boardWriter;
-	}
-
-
-
-	public String getBoardSubject() {
-		return boardSubject;
-	}
-
-
-
-	public void setBoardSubject(String boardSubject) {
-		this.boardSubject = boardSubject;
-	}
-
-
-
-	public String getBoardContent() {
-		return boardContent;
-	}
-
-
-
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
-
-
-
-	public Date getBoardDate() {
-		return boardDate;
-	}
-
-
-
-	public void setBoardDate(Date boardDate) {
-		this.boardDate = boardDate;
-	}
-
-
-
-	public int getBoardView() {
-		return boardView;
-	}
-
-
-
-	public void setBoardView(int boardView) {
-		this.boardView = boardView;
-	}
-
-
-
-	@Override
-	public String toString() {
-		return "TestVo [boardNum=" + boardNum + ", boardWriter=" + boardWriter + ", boardSubject=" + boardSubject
-				+ ", boardContent=" + boardContent + ", boardDate=" + boardDate + ", boardView=" + boardView + "]";
-	}
 	
 	
+	
+	/*
+	 * 
+	 * public TestVo() { super(); }
+	 * 
+	 * 
+	 * 
+	 * public TestVo(int boardNum, String boardWriter, String boardSubject, String
+	 * boardContent, Date boardDate, int boardView) { super(); this.boardNum =
+	 * boardNum; this.boardWriter = boardWriter; this.boardSubject = boardSubject;
+	 * this.boardContent = boardContent; this.boardDate = boardDate; this.boardView
+	 * = boardView; }
+	 * 
+	 * 
+	 * 
+	 * public int getBoardNum() { return boardNum; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardNum(int boardNum) { this.boardNum = boardNum; }
+	 * 
+	 * 
+	 * 
+	 * public String getBoardWriter() { return boardWriter; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardWriter(String boardWriter) { this.boardWriter =
+	 * boardWriter; }
+	 * 
+	 * 
+	 * 
+	 * public String getBoardSubject() { return boardSubject; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardSubject(String boardSubject) { this.boardSubject =
+	 * boardSubject; }
+	 * 
+	 * 
+	 * 
+	 * public String getBoardContent() { return boardContent; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardContent(String boardContent) { this.boardContent =
+	 * boardContent; }
+	 * 
+	 * 
+	 * 
+	 * public Date getBoardDate() { return boardDate; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardDate(Date boardDate) { this.boardDate = boardDate; }
+	 * 
+	 * 
+	 * 
+	 * public int getBoardView() { return boardView; }
+	 * 
+	 * 
+	 * 
+	 * public void setBoardView(int boardView) { this.boardView = boardView; }
+	 * 
+	 * 
+	 * 
+	 * @Override public String toString() { return "TestVo [boardNum=" + boardNum +
+	 * ", boardWriter=" + boardWriter + ", boardSubject=" + boardSubject +
+	 * ", boardContent=" + boardContent + ", boardDate=" + boardDate +
+	 * ", boardView=" + boardView + "]"; }
+	 * 
+	 */
 	
 	
 }
