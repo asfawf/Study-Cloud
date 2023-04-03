bd84e5f3d2fa1d465152e08b5881566cad4ee116
asfawf <soub0713@naver.com>
2023-04-04 01:46:29 +0900

    메일 전송 + 난수 비번 전송 성공
    오류 없음 + 추가 jsp 작업은 후에

---
+++ stc_source/src/main/java/study/cloud/stc/mailSender.java
@@ -0,0 +1,94 @@
+package study.cloud.stc;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ctc.wstx.shaded.msv_core.util.Util;
+
+@Controller
+@RequestMapping("/changemail")
+public class mailSender {
+	
+
+	@RequestMapping("/naver")
+	public ModelAndView changemailsend(ModelAndView mv,HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+		String proId = WebUtil.getProperty("mail_id");
+		String proPass = WebUtil.getProperty("mail_password");
+		String proEmail = WebUtil.getProperty("mail_email");
+		
+		System.out.println("proId: " + proId);
+		System.out.println("propass: " + proPass);
+		
+		String host = "smtp.naver.com";
+		final String username = proId;
+		final String password = proPass;
+		int port = 465;
+		
+		
+		// 메일 내용
+		
+		String change = "";
+		for (int i = 0; i < 12; i++) {
+			change += (char) ((Math.random() * 26) + 97);
+		}
+		
+		System.out.println();
+		System.out.println("pw1: "+ change);
+		
+		// 보내는 사람
+		String recipient  = proEmail;
+		
+		String subject = "임시 비밀번호 발급 메일 입니다.";
+		String body = "귀하의 변경된 비밀번호는 " + change + " 입니다.";
+
+		Properties props = System.getProperties();
+		
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.ssl.enable", true);
+		props.put("mail.smtp.ssl.trust", host);
+		
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() { 
+            String un=username;
+            String pw=password; 
+            protected PasswordAuthentication getPasswordAuthentication() { 
+                return new PasswordAuthentication(un, pw); 
+            } 
+        });
+		session.setDebug(true);
+		
+		Message mimeMessage = new MimeMessage(session); 
+	    mimeMessage.setFrom(new InternetAddress(proEmail)); 
+	    mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+	    mimeMessage.setSubject(subject); 
+	    mimeMessage.setText(body); 
+	    Transport.send(mimeMessage);
+	        
+	    mv.setViewName("/changemail");
+	    
+	   return mv;
+		
+	}
+
+}
