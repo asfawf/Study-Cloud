ef4b2f28b10b154f2b47598e98f450cca18a0c4a
seoanchoi <seoaneeee@naver.com>
2023-04-26 15:18:14 +0900

    회원가입 MailSender 수정

--- stc_source/src/main/java/study/cloud/stc/MailSender.java
+++ stc_source/src/main/java/study/cloud/stc/MailSender.java
@@ -3,8 +3,10 @@ package study.cloud.stc;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Properties;
+import java.util.Random;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -15,11 +17,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ctc.wstx.shaded.msv_core.util.Util;
@@ -30,9 +35,76 @@ import study.cloud.stc.member.model.vo.MemberVo;
 @Controller
 @RequestMapping("/sendmail")
 public class MailSender {
-	
-	@Autowired
-	MemberService service;
+
+    @Autowired
+    MemberService service;
+
+    @GetMapping("/joinemail")
+	public String joinemail(ModelAndView mv,HttpServletRequest request, HttpServletResponse response, @RequestParam("memEmail") String memEmail) throws Exception{
+		
+		String proId = WebUtil.getProperty("mail_id");
+		String proPass = WebUtil.getProperty("mail_password");
+		String proEmail = WebUtil.getProperty("mail_email"); // 보내는 사람
+
+		System.out.println("proId: " + proId);
+		System.out.println("propass: " + proPass);
+		System.out.println("sendTo: "+ memEmail);
+
+		String sendTo= memEmail;
+		String host = "smtp.naver.com";
+
+		final String username = proId;
+		final String password = proPass;
+		int port = 465;
+
+		// 메일 내용
+		String change = "";
+		for (int i = 0; i < 12; i++) {
+			change += (char) ((Math.random() * 26) + 97);
+		}
+
+		System.out.println();
+		System.out.println("인증코드: "+ change);
+		
+		System.out.println(memEmail);
+
+
+		// 받는 사람
+		String recipient  = sendTo;
+
+		String subject = "StudyCloud 회원가입 인증 코드입니다.";
+		String body = "StudyCloud를 방문해 주셔서 감사합니다.<br><br>" +
+                		"인증 코드는 " + change + "입니다.<br>" +
+                		"인증 코드를 이메일 인증 코드란에 기입하세요.";
+
+
+		Properties props = System.getProperties();
+		
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", port);
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.ssl.enable", true);
+		props.put("mail.smtp.ssl.trust", host);
+		
+		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() { 
+            String un=username;
+            String pw=password; 
+            protected PasswordAuthentication getPasswordAuthentication() { 
+                return new PasswordAuthentication(un, pw); 
+            } 
+        });
+		session.setDebug(true);
+		
+		Message mimeMessage = new MimeMessage(session); 
+	    mimeMessage.setFrom(new InternetAddress(proEmail)); 
+	    mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient)); 
+	    mimeMessage.setSubject(subject); 
+	    mimeMessage.setText(body); 
+	    Transport.send(mimeMessage);
+	  
+	    return "";
+		
+	}
 	
 	@GetMapping("/password")
 	public String passwordMailSend(ModelAndView mv,HttpServletRequest request, HttpServletResponse response, MemberVo vo) throws Exception{
